3f03223065d7f5a993f7eb4ed1861bbcfe883abe
github username <github email address>
2018-07-07 15:03:22 -0400

    added validation for first_name, last_name, city, state, letters only

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :accounts
   has_many :transactions, through: :accounts
   has_secure_password
-
+  validates :first_name, :last_name, :city, :state, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Only letters allowed" }
 end
