d035fa3f13ce8e66a69b384c635e3ec0982c472f
github username <github email address>
2018-08-30 18:02:55 -0400

    created next stock button with data-id 0 as a counter

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,9 +1,12 @@
 $(function(){
 
-
+  stockBrowser();
 })
 
   function stockBrowser(){
-    
+    $('#stock_browser').on('click', function(e){
+      debugger
+      console.log(this)
+    })
 
   }
