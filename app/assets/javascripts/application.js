16f2c0a2e47f7713959653a45e0348b13e3282ba
github username <github email address>
2018-08-30 16:49:26 -0400

    line 5-16, users.js, created class Account; added constructor function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery3
-//= jquery-turbolinks
 //= require jquery_ujs
 //= require activestorage
 //= require turbolinks
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,18 +2,30 @@ $(function(){
   showAccounts();
 });
 
+  class Account {
+    constructor(data){
+    this.id = data.id;
+    this.type_of_account = data.type_of_account;
+    this.balance = data.balance;
+    }
+
+    accountInformation() {
+      return `Your ${this.type_of_account} account has a balance of $${this.balance}.`
+    }
+
+  }
+
+
   function showAccounts(){
     $('#user_accounts').on("click", function(e){
       let $div = $('div.accounts');
       if ($('#user_accounts').text() === "View Accounts"){
-
       $('#user_accounts').text("Hide Accounts")
-      $.get("/users/" + this.dataset.id + "/accounts" + ".json", function(data){
-        $div.html("")
-        data.forEach(function(account){
-          $div.append('<br>' + '<a href=/accounts/' + `${account.id}` + '>' + `${account.type_of_account}` + '</a>' + '<br><br>$' + account.balance + '<br>')
-        });
-      });
+      $div.html("")
+        fetch("/users/" + $('#user_accounts')[0].dataset.id + "/accounts" + ".json")
+        .then((res) => res.json())
+        .then((data) => debugger)
+
     } else {
       $('#user_accounts').text("View Accounts")
       $div.html("")
