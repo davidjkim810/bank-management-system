fc5862f730621f9caf2a5d434751fd3d366e60c1
github username <github email address>
2018-08-30 18:37:58 -0400

    jquery get request in stocks.js line 10

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -6,7 +6,10 @@ $(function(){
     $('#stock_browser').on('click', function(e){
       let $stocks = $('div.stocks')
       $stocks.html("")
-      
+
+      $.get('/users/' + this.dataset.user_id + '/shares/' + this.dataset.stock_id, function(data){
+        
+      });
 
 
 
